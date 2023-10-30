412a0477025218858e1d22a3309b7331759f8a46
Adel <moussaadel97@gmail.com>
2023-10-30 07:09:36 -0300

    made more modular

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 import tkinter as tk
+from dotenv import load_dotenv
+from api import get_latex_equation
+from settings import initialize_settings
+from tkinter import messagebox
+
+
+def convert_prompt(text_input_widget, text_output_widget):
+    # Extract the input prompt from the Text widget
+    prompt = text_input_widget.get("1.0", tk.END).strip()
+
+    # Get the LaTeX equation using the extracted prompt
+    latex_equation = get_latex_equation(prompt)
+
+    # Clear the output Text widget and insert the LaTeX equation
+    text_output_widget.delete("1.0", tk.END)
+    text_output_widget.insert(tk.END, latex_equation)
+
+def copy_to_clipboard(output_widget, root):
+    try:
+        root.clipboard_clear()  # Clear the clipboard
+        text_to_copy = output_widget.get("1.0", tk.END).strip()  # Get text from output
+        root.clipboard_append(text_to_copy)  # Append text to the clipboard
+        messagebox.showinfo("Success", "Output copied to clipboard!")  # Optional: show a confirmation message
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to copy: {e}")
+
+def set_api_key(root):
+    # This function will be called when the user selects the option to set the API key
+    api_key = simpledialog.askstring("API Key", "Enter your API key:", parent=root)
+    if api_key:
+        # Here you would normally save this API key to a file or an environment variable
+        print(api_key)  # For demonstration purposes, we're just printing it
+        # For example, you might use: openai.api_key = api_key
+
+
 def main():
+    load_dotenv()  # Load environment variables from a .env file
+    initialize_settings()  # Load non-sensitive settings from a config file
+
     root = tk.Tk()
     root.title("Equation Converter")
 
@@ -19,14 +57,11 @@ def main():
     convert_button = tk.Button(root, text="Convert", command=lambda: convert_prompt(text_input, text_output))
     convert_button.pack()
 
-    root.mainloop()
+    # Button to copy output to clipboard
+    copy_button = tk.Button(root, text="Copy to Clipboard", command=lambda: copy_to_clipboard(text_output, root))
+    copy_button.pack()
 
-def convert_prompt(text_input, text_output):
-    # Dummy function for now
-    input_text = text_input.get("1.0", tk.END)
-    output_text = f"Converted LaTeX: {input_text}"  # Placeholder for actual conversion logic
-    text_output.delete("1.0", tk.END)
-    text_output.insert(tk.END, output_text)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
---
+++ openai_key.py
@@ -0,0 +1,3 @@
+import openai
+
+openai.api_key = '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'
